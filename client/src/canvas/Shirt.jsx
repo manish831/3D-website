31feef00944a6009b7dcfa1086bc8af15d96bb2a
Manish Agrahari <manish.agrahari2015@gmail.com>
2023-12-04 17:41:51 +0530

    canvas working is fixed

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,18 +1,21 @@
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
-import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
 
-import state from '../store';
+import state from "../store";
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
 
   const stateString = JSON.stringify(snap);
 
@@ -25,17 +28,16 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
-        {snap.isFullTexture && (
-          <Decal 
+         {snap.isFullTexture && (
+          <Decal
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
             scale={1}
             map={fullTexture}
           />
         )}
-
         {snap.isLogoTexture && (
-          <Decal 
+          <Decal
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
@@ -47,7 +49,7 @@ const Shirt = () => {
         )}
       </mesh>
     </group>
-  )
-}
+  );
+};
 
-export default Shirt
+export default Shirt;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,20 +2,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
-import Backdrop from "./Backdrop";
+// import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
 const CanvasModel = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <Environment preset="city" />
-      <CameraRig>
-        <Backdrop />
-      <Center>
-        <Shirt />
-      </Center>
-      </CameraRig>
-    </Canvas>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <Environment preset="city" />
+        <CameraRig>
+          {/* <Backdrop /> */}
+          <Center>
+            <Shirt />
+          </Center>
+        </CameraRig>
+      </Canvas>
   );
 };
 
